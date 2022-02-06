35d94ec5fcd1669e003b6f7931e47b6c71bda21b
seenachacko <seenachacko119@gmail.com>
2022-02-06 21:25:39 +0100

    make changes

--- src/components/About.js
+++ src/components/About.js
@@ -16,12 +16,10 @@ export default function About() {
           />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-2xl text-1xl mb-2 font-small text-white">
+          <h1 className="title-font sm:text-4xl text-1xl mb-4 font-meadium text-white">
             Seena Chacko
-            Front-end Developer
           </h1>
           <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Seena Chacko
             Front-end Developer
           </h2>
           <p className="mb-8 leading-relaxed">
